14ca5c8aef19d0508c8b929d13072f344123b977
Sadiq Saleem <106178827+Sadiq008@users.noreply.github.com>
2025-01-26 19:59:14 +0530

    index.js

--- Server/src/index.js
+++ Server/src/index.js
@@ -30,10 +30,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../Client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
 
